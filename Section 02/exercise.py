d990ee7a949cebe6ca4936f7546bf5a365fb8022
M9X8RCSO9E <46058828+M9X8RCSO9E@users.noreply.github.com>
2025-01-26 11:11:51 -0500

    Section 2 complete

--- Section 02/exercise.py
+++
@@ -1,6 +0,0 @@
-# This code defines a name and prints it
-name = str("Mr. Skywalker")
-age = int(44)
-print(name)
-type(name)
-type(age)
---
+++ Section 02/final_exercise.py
@@ -0,0 +1,6 @@
+#Currency Converter
+usd_amount = 100
+exchange_rate = .85
+eur_amount = usd_amount * exchange_rate
+eur_amount = int(eur_amount)
+print("The amount in EUR is:", eur_amount)
